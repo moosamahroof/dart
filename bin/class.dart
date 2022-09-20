a953e30d523c35b89b7541a247eac49c4afad2d5
mahroof <mahroofanchukandan@gmail.com>
2022-09-20 16:06:54 +0530

    Initial commit

---
+++ bin/class.dart
@@ -0,0 +1,18 @@
+class students{
+
+late string name;
+late int age;
+late int phone;
+static string cource="flutter";
+static string institute='luminar Technolab';
+
+}
+
+void main()
+
+students std1=students();
+
+print("student 1 details");
+print("name    : ${std1.name="mahroof"}");
+print("Age     : ${std1.age=21}");
+print('phone    ; ${std1.number=7777000774}');
