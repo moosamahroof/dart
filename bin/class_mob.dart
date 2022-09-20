828f3dde20a9214d5eacba243d442aa53210888d
mahroof <mahroofanchukandan@gmail.com>
2022-09-21 00:10:50 +0530

    clas

---
+++ bin/class_mob.dart
@@ -0,0 +1,46 @@
+class mobiles{
+  late String model;
+  late int date;
+  late int storage;
+  static String brand = "samsung";
+  static String processor = "snapDragon";
+}
+  void main()
+  {
+  mobiles mob1=mobiles();
+
+  print("first mobiles details");
+  print("model is :${mob1.model="s22 ultra"} ");
+  print("date of indroduce is :${mob1.date=2022}");
+  print("the storage is : ${mob1.storage=256}");
+  print("The chipset is  : ${mobiles.processor}");
+  print("the brand is :${mobiles.brand}");
+
+  print("*******************************************");
+  print("first mobiles details");
+  print("model is :${mob1.model="s22 "} ");
+  print("date of indroduce is :${mob1.date=2022}");
+  print("the storage is : ${mob1.storage=256}");
+  print("The chipset is  : ${mobiles.processor="enoyx"}");
+  print("the brand is :${mobiles.brand}");
+
+  print("*******************************************");
+  print("first mobiles details");
+  print("model is :${mob1.model="note 20"} ");
+  print("date of indroduce is :${mob1.date=2022}");
+  print("the storage is : ${mob1.storage=256}");
+  print("The chipset is  : ${mobiles.processor}");
+  print("the brand is :${mobiles.brand}");
+
+  print("*******************************************");
+
+
+
+
+
+
+
+
+
+  }
+
