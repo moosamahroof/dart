8be868665463df1da78d4b5ca4e43c97ff9a04ba
mahroof <mahroofanchukandan@gmail.com>
2022-09-21 23:19:50 +0530

    function1

---
+++ bin/function.dart
@@ -0,0 +1,23 @@
+void add() {
+             int a = 10,b = 20;
+             print("sum is   :${a + b}");
+             }
+void diff(){
+            int a = 30,b = 20;
+            print("diffrence is   :${a - b}");
+              }
+void div() {
+            int a = 10,b = 20;
+            print("division is   :${a / b}");
+             }
+void sub() {
+  int a = 10,b = 20;
+  print("substraction is   :${a * b}");
+}
+void main(){
+     add();
+      diff();
+      div();
+      sub();
+
+}
