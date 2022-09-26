f71bac075c9052ad1b3e872d5e6d71a0b232e735
mahroof <mahroofanchukandan@gmail.com>
2022-09-26 16:43:20 +0530

    if_else-ladder

---
+++ bin/els_if_ladder.dart
@@ -0,0 +1,25 @@
+void main(){
+
+  var a=10,
+      b=20,
+      c=30,
+      d=40;
+
+if(a>b && a>c && a>d){
+    print("a is greater");
+  }
+   else if(b>a && b>c && b>d)
+{
+     print("b is greater");
+
+}
+   else if(c>a && c>b && c>d){
+  print("c is greater");
+   }
+  else {
+        print(" d is greater");
+     }
+
+
+
+}
