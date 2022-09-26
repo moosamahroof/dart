f597b26d920aa184fdfc10228b5d09392f0bda7c
mahroof <mahroofanchukandan@gmail.com>
2022-09-26 15:20:55 +0530

    switch-case

---
+++ bin/switch_case.dart
@@ -0,0 +1,44 @@
+void main(){
+
+  var month=9;
+
+  switch(month) {
+    case 1 :
+      print("jan");
+      break;
+    case 2 :
+      print("feb");
+      break;
+    case 3 :
+      print("mar");
+      break;
+    case 4 :
+      print("april");
+      break;
+    case 5 :
+      print("may");
+      break;
+    case 6 :
+      print("june");
+      break;
+    case 7 :
+      print("july");
+      break;
+    case 8 :
+      print("aug");
+      break;
+    case 9 :
+      print("sep");
+      break;
+    case 10 :
+      print("oct");
+      break;
+    case 11 :
+      print("nov");
+      break;
+    case 12 :
+      print("des");
+      break;
+  }
+  }
+
