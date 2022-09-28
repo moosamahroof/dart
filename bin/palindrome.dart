df5d59880a91f8266630ab0c967116c9e1969794
mahroof <mahroofanchukandan@gmail.com>
2022-09-28 15:30:08 +0530

    palindrome

---
+++ bin/palindrome.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+void main(){
+  int temp,rev=0,rem;
+  print("enter a number");
+  int num = int.parse(stdin.readLineSync()!);
+  temp=num;
+    while(num>0){
+    rem = num % 10;
+    rev = rev * 10 + rem;
+    num= num ~/ 10;
+  }
+ if (rev==temp){
+
+   print("palindrome number");
+
+ }else {
+   print("not palindrome");
+ }
+
+}
